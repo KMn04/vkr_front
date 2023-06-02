c022df5244465b04772a8104a706733759ccd71a
Kmn04 <21zaarin01@gmail.com>
2023-06-02 07:26:02 +0300

    профиль

--- src/services/ProfileServices.ts
+++ src/services/ProfileServices.ts
@@ -1,11 +1,40 @@
 import { IUserItem } from '../stores/usersStore';
-import { IProjectTask } from '../types/Projects';
-import { ITicket } from '../types/Ticket';
 import { ApiConnection } from './ApiConnection';
+
+export interface IUserProfile {
+  userId: number;
+  secondName: string;
+  firstName: string;
+  middleName?: string;
+  login: string;
+  email: string
+}
+
+export interface IUserProfileUpdate {
+  userId: number;
+  secondName: string;
+  firstName: string;
+  thirdName?: string;
+  login: string;
+  email: string;
+}
 
 class ProfileServices {
   static get RoutePrefix(): string {
     return 'profile';
+  }
+
+  static async getAccount(): Promise<IUserProfile> {
+    const response = await ApiConnection.get(this.RoutePrefix + '/');
+    return response.data
+  }
+
+  static async update(request: Partial<IUserProfileUpdate>): Promise<void> {
+    const response = await ApiConnection.put(this.RoutePrefix, request)
+  }
+
+  static async deleteUser(): Promise<void> {
+    await ApiConnection.delete(this.RoutePrefix)
   }
 
   static async getAllUsers(): Promise<IUserItem[]> {
